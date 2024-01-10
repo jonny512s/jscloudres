53a755be6cce189664eb5c2900f72b12ee8d8633
jonny512s <jonny512@utexas.edu>
2024-01-10 12:48:18 -0600

    ensures view count is loaded and parsed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const counter = document.querySelector(".counter-number");
-//lambda function trigger
-async function updateCounter(){
-    //lambda function URL
-    let response = await fetch("https://dtsbjqsswgfgzl6uvilaaljt4a0kukyo.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = `Site View Count: ${data}`;
-}
-updateCounter();
+document.addEventListener('DOMContentLoaded', (event) => {
+    const counter = document.querySelector(".counter-number");
+    //lambda function trigger
+    async function updateCounter(){
+        //lambda function URL
+        let response = await fetch("https://dtsbjqsswgfgzl6uvilaaljt4a0kukyo.lambda-url.us-east-1.on.aws/");
+        let data = await response.json();
+        counter.innerHTML = `Site View Count: ${data}`;
+    }
+    updateCounter();
+});
