d157d960cf788b18bfd364ec296db0d98dd12993
jonny512s <jonny512@utexas.edu>
2024-01-11 16:21:37 -0600

    Ensure  trigger properly interacts w lambda

--- server/index.js
+++ server/index.js
@@ -5,17 +5,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
     async function updateCounter(attempt = 1) {
         try {
             const cacheBuster = new Date().getTime();
-            let response = await fetch(`https://dtsbjqsswgfgzl6uvilaaljt4a0kukyo.lambda-url.us-east-1.on.aws/?${cacheBuster}`, {     
+            let response = await fetch(`https://dtsbjqsswgfgzl6uvilaaljt4a0kukyo.lambda-url.us-east-1.on.aws/?${cacheBuster}`, {
                 headers: {
-                'Cache-Control': 'no-cache'
-            }
-        });
+                    'Cache-Control': 'no-cache'
+                }
+            });
+
             if (!response.ok && attempt < 3) { // If response is not OK and less than 3 attempts
                 setTimeout(() => updateCounter(attempt + 1), 2000); // Retry after 2 seconds
                 return;
             }
+
             let data = await response.json();
-            counter.innerHTML = `Site View Count: ${data}`;
+            // Extracting the 'views' value from the response
+            counter.innerHTML = `Site View Count: ${data.views}`;
         } catch (error) {
             counter.innerHTML = 'Could not load views'; // Error message if fetching fails
             console.error('Error fetching counter data:', error);
