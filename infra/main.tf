f0502fdbdf8319f76a12262c60064db26b6e83a6
jonny512s <jonny512@utexas.edu>
2024-01-15 20:52:50 -0600

    Access and secret key & .gitignore for terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,14 +1,37 @@
-terraform {
-    required_providers {
-      aws = {
-        version = ">= 4.9.0"
-        source = "hashicorp/aws"
+// Create lambda function
+resource "aws_lambda_function" "cloudResViewsFunc" {
+    filename         = data.archive_file.zip_the_python_code.output_path
+    source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
+    function_name    = "cloudResViewsFunc"
+    role             = aws_iam_role.iam_for_lambda.arn
+    handler          = "cloudResViewsFunc.handler"
+    runtime          = "python3.8"
+}
 
-      }
-    }
+// Create IAM role for lambda function
+resource "aws_iam_role" "iam_for_lambda" {
+    name = "iam_for_lambda"
+    assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+  ]
 }
-provider "aws" {
-    access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-    secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-    region : 'us-east-1'
+EOF
 }
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/cloudResViewsFunc.py"
+  output_path = "${path.module}/lambda/cloudResViewsFunc.zip"
+}
+
+// upload lambda function trough zip file
--- infra/provider.tf
+++ infra/provider.tf
@@ -1,31 +1,18 @@
-// Create lambda function
-resource "aws_lambda_function" "cloudResViewsFunc" {
-    filename         = data.archive_file.zip_the_python_code.output_path
-    source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
-    function_name    = "cloudResViewsFunc"
-    role             = aws_iam_role.iam_for_lambda.arn
-    handler          = "cloudResViewsFunc.handler"
-    runtime          = "python3.8"
+terraform {
+    required_providers {
+      aws = {
+        version = ">= 4.9.0"
+        source = "hashicorp/aws"
+
+      }
+    }
 }
 
-// Create IAM role for lambda function
-resource "aws_iam_role" "iam_for_lambda" {
-    name = "iam_for_lambda"
-    assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            },
-            "Effect": "Allow",
-            "Sid": ""
-        }
-  ]
-}
-EOF
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
-// upload lambda function trough zip file
+provider "aws" {
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    region     = "us-east-1"
+}
