31997a240c1e8ab85f3d6a635fe9767c41454b40
jonny512s <jonny512@utexas.edu>
2024-01-17 22:08:22 -0600

    Lambda API updates

--- server/index.js
+++ server/index.js
@@ -1,14 +1,8 @@
     const counter = document.querySelector(".counter-number");
     counter.innerHTML = 'Loading views...'; // Initial loading message
     async function updateCounter() {
-        try {
-            const cacheBuster = new Date().getTime();
-            let response = await fetch("https://vodcluk3rhvrk5xqsi5obehah40jrhlm.lambda-url.us-east-1.on.aws/?cacheBust=${cacheBuster}");
-            let data = await response.json();
-            counter.innerHTML = `Site View Count: ${data}`;
-        } catch (error) {
-            counter.innerHTML = 'Could not load views'; // Error message if fetching fails
-            console.error('Error fetching counter data:', error);
-        }
+        let response = await fetch("https://vodcluk3rhvrk5xqsi5obehah40jrhlm.lambda-url.us-east-1.on.aws/");
+        let data = await response.json();
+        counter.innerHTML = `Site View Count: ${data}`;
     }
     updateCounter();
