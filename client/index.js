e3d7dd41aaab129144d4bd89f8306ae6c0687172
jonny512s <jonny512@utexas.edu>
2024-01-17 23:17:39 -0600

    updates

--- client/index.js
+++ client/index.js
@@ -1,8 +1,26 @@
+document.addEventListener('DOMContentLoaded', (event) => {
     const counter = document.querySelector(".counter-number");
     counter.innerHTML = 'Loading views...'; // Initial loading message
-    async function updateCounter() {
-        let response = await fetch("https://dtsbjqsswgfgzl6uvilaaljt4a0kukyo.lambda-url.us-east-1.on.aws/");
-        let data = await response.json();
-        counter.innerHTML = `Site View Count: ${data.views}`;
+    async function updateCounter(attempt = 1) {
+        try {
+            const cacheBuster = new Date().getTime();
+            let response = await fetch(`https://dtsbjqsswgfgzl6uvilaaljt4a0kukyo.lambda-url.us-east-1.on.aws/?${cacheBuster}`, {
+                headers: {
+                'cache-control': 'no-cache'
+            }
+        });
+            if (!response.ok && attempt < 3) { // If response is not OK and less than 3 attempts
+                setTimeout(() => updateCounter(attempt + 1), 2000); // Retry after 2 seconds
+                return;
+            }
+
+            let data = await response.json();
+            // Extracting the 'views' value from the response
+            counter.innerHTML = `Site View Count: ${data.views}`;
+        } catch (error) {
+            counter.innerHTML = 'Could not load views'; // Error message if fetching fails
+            console.error('Error fetching counter data:', error);
+        }
     }
-    updateCounter();
+        updateCounter();
+        });
