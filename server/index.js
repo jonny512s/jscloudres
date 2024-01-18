037264a8f0cc04b058af40b227dbbcbd491b50cb
jonny512s <jonny512@utexas.edu>
2024-01-17 21:14:39 -0600

    update lambda URL to IAC deployed lambda

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     async function updateCounter(attempt = 1) {
         try {
             const cacheBuster = new Date().getTime();
-            let response = await fetch(`https://dtsbjqsswgfgzl6uvilaaljt4a0kukyo.lambda-url.us-east-1.on.aws/${cacheBuster}`, {
+            let response = await fetch(`https://vodcluk3rhvrk5xqsi5obehah40jrhlm.lambda-url.us-east-1.on.aws/${cacheBuster}`, {
                 headers: {
                     'cache-control': 'no-cache'
                 }
